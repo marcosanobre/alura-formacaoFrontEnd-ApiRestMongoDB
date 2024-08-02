bca0905abdb5c467e98a06e8ec08338910c0934c
marcosanobre <marconobre@gmail.com>
2024-08-02 11:36:59 -0300

    Primeiros passos

---
+++ server.js
@@ -0,0 +1,20 @@
+import http from "http";
+
+const PORT = 3000;
+
+const rotas = {
+    "/": "Curso de Node.JS",
+    "/livros": "Entramos na rota Livros"
+}
+
+const server = http.createServer( (req,res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end( rotas[req.url] );
+});
+
+server.listen( PORT, () => {
+    console.log( "Servidor escutando" );
+});
+
+
+
